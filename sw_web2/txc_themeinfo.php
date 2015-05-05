e58c72a84e7fa902d4ed6a20c713151b5d2ead00
mambax7 <mambax7@gmail.com>
2015-05-05 09:32:21 -0400

    Original files

---
+++ sw_web2/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "sw_web2";
+	$txc_themeinfo["author"] = "ffxk";
+	$txc_themeinfo["author_url"] = "http://www.psycn.net/";
+	$txc_themeinfo["detail_url"] = "http://ffxk.com/modules/wordpress/255.htm";
+	$txc_themeinfo["download_url"] = "http://ffxk.com/modules/fileup/";
+?>
